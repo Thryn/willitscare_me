b0f9983572fa36e35ee6156eb655d40da082b129
thryn <thryn@thryn-macbookpro.roam.corp.google.com>
2015-04-06 13:18:51 -0700

    Replaces output with elastic search; rewrites function to use elastic search; unbinds messages in view from query so that the message is pulled from the backend

--- js/app.js
+++ js/app.js
@@ -6,61 +6,40 @@ myApp.controller('myController', ['$scope', '$http', function($scope, $http){
     $scope.notFound = false;
     $scope.scaryThing = '';
     $scope.requestedMovie = '';
-
-    $scope.isItScary = function () {
-      $scope.clicked = true;
-      // $scope.scary = !$scope.scary;
-    };
     
     $scope.lookUpMovie = function () {
         $scope.clicked = true;
-        $scope.moviePath = ["output", $scope.requestedMovie.substring(0,2), $scope.requestedMovie.substring(2,4), $scope.requestedMovie + ".json"].join('/');
-        console.log($scope.moviePath);
-        $http.get($scope.moviePath).
+        var query = {
+          query: {
+            bool: {
+              must: $scope.requestedMovie.split(/\s+/).map(function (word) { return {term: {title: word.toLowerCase()}}; })
+            }
+          }
+        };
+        var searchPath = "http://localhost:9200/movies/movies/_search";
+        $http.post(searchPath, query).
           success(function(data, status, headers, config) {
-            // this callback will be called asynchronously
-            // when the response is available
-            $scope.notFound = false;
-            $scope.scary = data.keywords.indexOf($scope.scaryThing) !== -1;
-            console.log($scope.scaryThing);
-            console.log(data);
-
+            if (data.hits.hits.length > 0) {
+              $scope.notFound = false;
+              var re = new RegExp($scope.scaryThing, "gi");
+              $scope.scary = data.hits.hits[0]._source.keywords.some(function (keyword) {
+                  return keyword.search(re) != -1;
+                }
+              );
+              $scope.returnedMovie = data.hits.hits[0]._source.title;
+              $scope.returnedKeyword = $scope.scaryThing + "s";
+            } else {
+              $scope.notFound = true;              
+            }
           }).
           error(function(data, status, headers, config) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-            console.error(data);
-            $scope.notFound = true;
+            // no-op
           }).finally(function () {
-              // $scope.scaryThing = '';
-              // $scope.requestedMovie = '';
+            // $scope.scaryThing = '';
+            // $scope.requestedMovie = '';
           });
-
     }
-
-    // $scope.lookUpMoviehttp()
-
-
-//function for checking if something is scary:
-    $scope.willItScareMe = function(movie) {
-    //look up movie from user input
-        //if movie not found
-            //return not found message
-        //if movie found
-            //return keywords for that movie
-            //check if any keywords are a match against the user input of scaryThing (substring search)
-                //if yes 
-                    //return scary=true
-                    //update ui
-                //if no 
-                    //return scary=false
-                    //update ui
-        //clear input field
-    }
-    
 }]);
 
 //clear form after query
-//search for substring of keyword
-//take year out of movies (search for substring?)
-//make scary things able to be capitalized
+//fix: false positive if no keyword is selected
